8a2a88ac96c5db05219628475bba54de711007bf
gldana1 <gldana1@gmail.com>
2023-03-23 20:35:18 +0200

    removed api

--- app.py
+++ app.py
@@ -6,10 +6,19 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+
 connection = sqlite3.connect("movies.db")
 db = connection.cursor()
-db.execute("SELECT * FROM users")
+rows = db.execute("SELECT * FROM users").fetchall()
 
+print(rows)
 #@app.route("/")
 #def homepage():
     #//TODO//
